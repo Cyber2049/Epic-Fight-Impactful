80f316c8fd8bdba62dc5769be063d789292f08d7
Nameless <clongki@hotmail.com>
2023-12-03 10:26:19 +0800

    fix server side screen shake

--- src/main/java/com/nameless/impactful/mixin/ClientLevelUtilMixin.java
+++ src/main/java/com/nameless/impactful/mixin/ClientLevelUtilMixin.java
@@ -1,11 +1,8 @@
 package com.nameless.impactful.mixin;
 
-import com.nameless.impactful.network.CameraShake;
-import com.nameless.impactful.network.NetWorkManger;
+import com.nameless.impactful.client.CameraEngine;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
@@ -18,10 +15,10 @@ import yesman.epicfight.api.utils.LevelUtil;
 import static yesman.epicfight.api.utils.LevelUtil.canTransferShockWave;
 
 @Mixin(LevelUtil.class)
-public class LevelUtilMixin {
+public class ClientLevelUtilMixin {
     @Inject(method = "circleSlamFracture(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/phys/Vec3;DZZZ)Z", at = @At("RETURN"), remap = false)
     private static void onCircleSlamFracture(LivingEntity caster, Level level, Vec3 center, double radius, boolean noSound, boolean noParticle, boolean hurtEntities, CallbackInfoReturnable<Boolean> cir){
-        if(!level.isClientSide()) {
+        if(level.isClientSide()) {
             Vec3 closestEdge = new Vec3(Math.round(center.x), Math.floor(center.y), Math.round(center.z));
             Vec3 centerOfBlock = new Vec3(Math.floor(center.x) + 0.5D, Math.floor(center.y), Math.floor(center.z) + 0.5D);
 
@@ -37,11 +34,7 @@ public class LevelUtilMixin {
             if (canTransferShockWave(level, blockPos, originBlockState)) {
                 int t = (int) Math.max(20, Math.min(15 * radius, 120));
                 float s = (float) Math.min(2 * radius, 12);
-                if (caster instanceof Player player) {
-                    for (ServerPlayer serverPlayer : player.level.getEntitiesOfClass(ServerPlayer.class, player.getBoundingBox().inflate(radius, 5, radius))) {
-                        NetWorkManger.sendToPlayer(new CameraShake(t, s), serverPlayer);
-                    }
-                }
+                CameraEngine.getInstance().shakeCamera(t,s);
             }
         }
     }
