94056c439f373944a203fb003428c7ad64c0da26
Nameless <clongki@hotmail.com>
2024-07-14 11:28:11 +0800

    fix parry shake add better third person compat

--- src/main/java/com/nameless/impactful/mixin/BTPCompat/MixinMouseHandler.java
+++ src/main/java/com/nameless/impactful/mixin/BTPCompat/MixinMouseHandler.java
@@ -1,12 +1,8 @@
 package com.nameless.impactful.mixin.BTPCompat;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHandler;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.util.SmoothDouble;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import yesman.epicfight.client.ClientEngine;
